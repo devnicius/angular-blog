536e471c0a011d7943176b5cb471748ad5c80b9d
vinicius.oliveira <vinicius.oliveira@movemais.com>
2024-04-25 17:50:01 -0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { HomeComponent } from './pages/home/home.component';
     MenuTitleComponent,
     BigCardComponent,
     SmallCardComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
